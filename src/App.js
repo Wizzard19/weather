052b73d7b94924a93bf75259e00fd494f49f43f8
Alejandro Spitale <alejandro.spitale@outlook.com>
2023-02-05 21:32:24 -0300

    Icons integration in progress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, {useEffect, useState} from 'react'
 import './App.css';
+import Icons from './Components/Icons';
 
 function App() {
   const [search, setSearch] = useState('');
   const [values, setValues] = useState('');
   const [icon, setIcon] = useState('');
 
-  const URL = `http://api.weatherapi.com/v1/forecast.json?key=9f88a08e2e31432bb00154158233001&q=cordoba&days=2&lang=es`
+  const URL = `http://api.weatherapi.com/v1/forecast.json?key=9f88a08e2e31432bb00154158233001&q=Santiago+templ&lang=es`
 
   const getData = async () => {
     await fetch(URL)
     .then(response => {return response.json()})
     .then( data => {
-      console.log(data)
+      if(data.error){
+        setValues(false)
+        document.write('Error en la busqueda ', '"' + data.error.message + '"')
+        console.log(data.error.code, data.error.message)
+      } else {
+      console.log(data);
+      setIcon(data.current.condition.icon)
+      setValues(data)
+      
+      }
     })
     .catch(error => {
       console.log(error)
@@ -20,12 +30,16 @@ function App() {
   }
 
   const handleSearch = (e)=> {
-    console.log(e.target.value)
+    if (e.key === 'Enter'){
+      setSearch(e.target.value)
+      console.log(e.target.value)
+    }
   }
   useEffect(()=>{
     getData()
-  },[])
+  },[search]) //eslint-disable-line react-hooks/exhaustive-deps
   return (
+    <>
     <div className='container'>
       <h1>SPITALAN Weather App</h1>
       <div className='row'>
@@ -35,10 +49,32 @@ function App() {
         autoFocus
         >
         </input>
+        
       </div>
     </div>
+
+    <div className='card'>
+      {(values) ? (
+        <div className='card-container'>
+          <h2 className='citi-name'>{values.location.name}</h2>
+          <p className='temp'>{values.current.temp_c.toFixed(0)}&deg;</p>
+          <img className='icon' src={Icons(icon)} alt='icon-weather'/>
+          <div className='card-footer'>
+           <p className='temp-max-min'>{values.forecast.forecastday[0].day.maxtemp_c.toFixed(0)}&deg;</p>
+           <p className='temp-max-min'>{values.forecast.forecastday[0].day.mintemp_c.toFixed(0)}&deg;</p>
+            
+          </div>
+
+        </div>
+      ) : (
+        <h2>Ciudad no encontrada</h2>
+        
+      )}
+    </div>
+    </>
   )
 }
 
-export default App
+export default App;
+
 
--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -1,9 +1,28 @@
-import React from 'react'
 
-const Icons = () => {
-  return (
-    <div>Icons</div>
-  )
+
+const Icons = (icon) => {
+  switch (icon) {
+    case 'Soleado':
+      icon='weather-icons/clear-day.svg'
+      console.log('Limpio dia')
+      break;
+      
+      case 'Nublado':
+        icon='weather-icons/cloudy.svg'
+        console.log('Limpio dia')
+        break;
+
+        case 'Llovizna':
+          icon='weather-icons/cloudy.svg'
+          console.log('Limpio dia')
+          break;
+    
+  
+    default:
+      icon='weather-icons/clear-day.svg'
+      console.log('default')
+  }
+  return icon
 }
 
 export default Icons
